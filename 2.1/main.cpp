9b90ed7c17af44e4bcfefa4b9df858323474b8fe
Tran Phong <tranphong96.hbk@gmail.com>
2019-01-20 15:58:52 +0700

    Add ex 2.1

---
+++ 2.1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	int x{1};
+	int y(2);
+	int m;
+	m = 3;
+	std::cout << "X is :" << x << std::endl;
+	std::cout << "Y is :" << y << std::endl;
+	std::cout << "M is :" << x << std::endl;
+	std::cout << "Size of integer: " << sizeof(int) << std::endl;
+	return 0;
+}
